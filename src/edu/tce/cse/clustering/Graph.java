883457a9c5145642042fab257a808edd0f728c65
Mukuntha Priya <mukuntha.priya.sr@gmail.com>
2015-09-06 16:15:00 +0530

    adding inital clustering code and rough code for choosing rep points for cluster

--- src/edu/tce/cse/clustering/Graph.java
+++ src/edu/tce/cse/clustering/Graph.java
@@ -1,25 +1,30 @@
 package edu.tce.cse.clustering;
 
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.BitSet;
+
+
 
 public class Graph<E extends Node> {
 	List<E> V;
 	Map<E, List<Edge>> adjList;
-	Graph(List<E> nodes){
+	public final static int NTHREAD = Runtime.getRuntime().availableProcessors();
+	public Graph(List<E> nodes){
 		V = new ArrayList<E>();
 		adjList = new HashMap<E, List<Edge>>();
 		V.addAll(nodes);
 		adjList.put(nodes.get(0), new ArrayList<Edge>());
 		for(int j=1; j<nodes.size(); j++){
 			adjList.put(nodes.get(j), new ArrayList<Edge>());
-			//float weight = findEdgeWeight(nodes.get(0), nodes.get(j));
-			float weight = j*0.1f;
-			//System.out.print("0 "+j+" "+weight+" ");
+			float weight = findEdgeWeight(nodes.get(0), nodes.get(j));
 			Edge e = new Edge(nodes.get(0), nodes.get(j), weight);
 			adjList.get(nodes.get(0)).add(e);
 			e = new Edge(nodes.get(j), nodes.get(0), weight);
@@ -27,9 +32,7 @@ public class Graph<E extends Node> {
 		}
 		for(int i=1; i<nodes.size(); i++){
 			for(int j=i+1; j<nodes.size(); j++){
-				//float weight = findEdgeWeight(nodes.get(i), nodes.get(j));
-				float weight = 0.1f*(j-i);
-				//System.out.print(i+" "+j+" "+weight+" ");
+				float weight = findEdgeWeight(nodes.get(i), nodes.get(j));
 				Edge e = new Edge(nodes.get(i), nodes.get(j), weight);
 				adjList.get(nodes.get(i)).add(e);
 				e = new Edge(nodes.get(j), nodes.get(i), weight);
@@ -40,7 +43,105 @@ public class Graph<E extends Node> {
 	public float findEdgeWeight(E node1, E node2){
 		return node1.findEdgeWEight(node2);
 	}
-	
-	
+	public void sparsify(float e){
+		Thread[] threads = new Thread[NTHREAD];
+		List<DocNode> myList;
+		int share = (int)Math.ceil(V.size()/NTHREAD);
+		System.out.println("");
+		System.out.println("");
+		System.out.println("After sparsification: ");
+		int d = V.size();
+		int toRetain = (int)Math.abs(Math.pow(d, e));
+		for (int i = 0; i < NTHREAD; i++) {
+			if(i!=NTHREAD-1)
+				myList = (List<DocNode>)V.subList(i*share, (i+1)*share);
+			else
+				myList = (List<DocNode>)V.subList(i*share, V.size());
+			threads[i] = new Thread(new SparsifierRunnable(i, adjList, myList, toRetain));
+			threads[i].start();
+		}
+		for (Thread thread : threads) {
+			try {
+				//A Wait for Joining all threads merging in a single Iteration
+				thread.join();
 
+			} catch (InterruptedException ee) {
+				ee.printStackTrace();
+			}
+		}
+		/*for(int i=0; i<V.size(); i++){
+			List<Edge> list=adjList.get(V.get(i));
+			for(int j=0; j<list.size(); j++){
+				System.out.print("Edge ("+V.get(i).nodeID+","+list.get(j).getDst().nodeID+") ");
+			}
+			System.out.println("");
+			//System.out.println(" ");
+		}*/
+	}
+	public List<List<DocNode>> findConnectedComponents(){
+		BitSet bs = new BitSet(V.size());
+		int count = 1;
+		List<List<DocNode>> clusters=new ArrayList<List<DocNode>>();
+		List<DocNode> cluster;
+		for(int i=0; i<V.size(); i++){
+			if(!bs.get(i)){
+				System.out.println(" ");
+				System.out.println("Component "+count+":");
+				cluster=new ArrayList<DocNode>();
+				findComponent(bs, (DocNode)V.get(i), cluster);
+				clusters.add(cluster);
+				count++;
+				
+			}
+		}
+		return clusters;
+	}
+	public void findComponent(BitSet bs, DocNode v, List<DocNode> cluster){
+		System.out.print(v.nodeID+" ");
+		bs.set((int)v.nodeID);
+		cluster.add(v);
+		for(int j=0; j<adjList.get(v).size(); j++){
+			DocNode neighbour = (DocNode)adjList.get(v).get(j).getDst();
+			if(!bs.get((int)neighbour.nodeID)){
+				findComponent(bs, neighbour, cluster);
+			}
+		}
+	}
+	
 }
+
+class WeightComparator implements Comparator{
+	public int compare(Object i, Object j){
+		if(((Edge)i).getWeight()>((Edge)j).getWeight())
+			return -1;
+		else if(((Edge)i).getWeight()==((Edge)j).getWeight())
+			return 0;
+		return 1;
+	}
+}
+class SparsifierRunnable<E extends Node> implements Runnable{
+	int id;
+	Map<E, List<Edge>> adjList;
+	List<E> myNodes;
+	int toRetain;
+	public SparsifierRunnable(int id, Map<E, List<Edge>> adjList, List<E> myList, int toRetain){
+		this.id=id;
+		this.adjList=adjList;
+		this.myNodes=myList;
+		this.toRetain = toRetain;
+	}
+	public void run(){
+		for(int i=0; i<myNodes.size(); i++){
+			List<Edge> list=adjList.get(myNodes.get(i));
+			Collections.sort(list, new WeightComparator());
+			int d=list.size();
+			list = list.subList(0, toRetain);
+			//System.out.println(list.size());
+			adjList.put(myNodes.get(i), list);
+			//list=adjList.get(myNodes.get(i));
+			//System.out.println("For "+myNodes.get(i).nodeID);
+			
+		}
+	}
+}
+
--- src/edu/tce/cse/clustering/InitialClusterer.java
+++ src/edu/tce/cse/clustering/InitialClusterer.java
@@ -5,23 +5,29 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.PriorityQueue;
 
-import edu.tce.cse.betweenness.*;
 public class InitialClusterer {
 	public final static int NTHREAD = Runtime.getRuntime().availableProcessors(); // Number of threads to create
 	List<DocNode> docs;
+	Graph<DocNode> graph;
 	public InitialClusterer(List<DocNode> list){
 		docs = list;
 	}
-	public Graph<DocNode> findCentrality(){
-		Graph<DocNode> graph = new Graph(docs);
+	public void findCentrality(){
+		graph = new Graph(docs);
+		//MODIFY SPARSIFICATION EXPONENT HERE
+		graph.sparsify(0.2f);
 		Thread[] threads = new Thread[NTHREAD];
-		int share=docs.size()/NTHREAD;
+		int share=(int)Math.ceil(docs.size()/NTHREAD);
 		List<DocNode> myList;
 		//DocNode[] minNodes = new DocNode[NTHREAD];
-		PriorityQueue<DocNode>[] queues = new FibonacciPriorityQueue[NTHREAD];
+		//PriorityQueue<DocNode>[] queues = new FibonacciPriorityQueue[NTHREAD];
+		PriorityQueue<DocNode>[] queues2 = new PriorityQueue[NTHREAD];
 		for(int source = 0; source<graph.V.size(); source++){
 			graph.V.get(source).level=0;
 			graph.V.get(source).priority=0;
@@ -32,7 +38,7 @@ public class InitialClusterer {
 					myList = graph.V.subList(i*share, (i+1)*share);
 				else
 					myList = graph.V.subList(i*share, graph.V.size());
-				threads[i] = new Thread(new ForwardPhaseRunnable(i, myList, graph, graph.V.get(source), new FibonacciPriorityQueue(), queues));
+				threads[i] = new Thread(new ForwardPhaseRunnable(i, myList, graph, graph.V.get(source), new PriorityQueue<DocNode>(10, new PriorityComparator()) , queues2));
 				threads[i].start();
 			}
 			for (Thread thread : threads) {
@@ -44,35 +50,31 @@ public class InitialClusterer {
 					e.printStackTrace();
 				}
 			}
-			/*for(int i=0; i<graph.V.size(); i++){
-				System.out.println(i+" "+graph.V.get(i).nodeID+" "+graph.V.get(i).priority);
-			}*/
+
 			while(true){
-				//System.out.println("----");
 				int count = 0;
 				int next = -1;
 				for(int i=0; i<NTHREAD; i++){
-					
-					if(queues[i]==null||queues[i].size()==0)
+
+					if(queues2[i]==null||queues2[i].size()==0)
 						count++;
 					else{
-						if(next==-1||queues[i].findMin().priority<queues[next].findMin().priority)
-							next=i;
-						//System.out.print(queues[i].size());
+						if(next==-1||queues2[i].peek().priority<queues2[next].peek().priority)
+						{next=i; 
+						}
 					}
 				}
-				if(count==NTHREAD)
+
+				if(count==NTHREAD||next==-1)
 					break;
-				
-				DocNode nextNode = queues[next].extractMin();
-				//System.out.println("Next: "+nextNode.nodeID);
-				PriorityQueue<DocNode>[] newQueues = new FibonacciPriorityQueue[NTHREAD];
+				DocNode nextNode = queues2[next].poll();
+				PriorityQueue<DocNode>[] newQueues = new PriorityQueue[NTHREAD];
 				for (int i = 0; i < NTHREAD; i++) {
 					if(i!=NTHREAD-1)
 						myList = graph.V.subList(i*share, (i+1)*share);
 					else
 						myList = graph.V.subList(i*share, graph.V.size());
-					threads[i] = new Thread(new ForwardPhaseRunnable(i, myList, graph, nextNode, queues[i], newQueues));
+					threads[i] = new Thread(new ForwardPhaseRunnable(i, myList, graph, nextNode, queues2[i], newQueues));
 					threads[i].start();
 				}
 				for (Thread thread : threads) {
@@ -84,13 +86,9 @@ public class InitialClusterer {
 						e.printStackTrace();
 					}
 				}
-				queues = newQueues;
-				/*for(int i=0; i<graph.V.size(); i++){
-					System.out.println(i+" "+graph.V.get(i).nodeID+" "+graph.V.get(i).priority);
-				}*/
+				queues2 = newQueues;
 			}
 			for(int i=0; i<graph.V.size(); i++){
-				//System.out.println(i+" "+graph.V.get(i).sig);
 				Set<Integer> iterator = graph.V.get(i).pred.keySet();
 				for(int j: iterator){
 					if(!edgesMap.containsKey(j)){
@@ -103,7 +101,7 @@ public class InitialClusterer {
 			Set<Integer> keyset = edgesMap.keySet();
 			List<Integer> levels = new ArrayList<Integer>();
 			levels.addAll(keyset);
-			Collections.sort(levels , new MyComparator());
+			Collections.sort(levels , new LevelComparator());
 			for(int level: levels){
 				for (int i = 0; i < NTHREAD; i++) {
 					if(i!=NTHREAD-1)
@@ -127,29 +125,97 @@ public class InitialClusterer {
 			for(int v=0; v<graph.V.size(); v++){
 				DocNode d = graph.V.get(v);
 				d.centrality+=(d.delta);
-				//System.out.print(d.delta+"--");
 				d.level=-1;
 				d.delta=0;
 				d.sig=0;
 				d.priority=Float.MAX_VALUE;
 				d.pred.clear();
 			}
-			/*for(int i=0; i<graph.V.size(); i++){
-			System.out.println(i+" "+graph.V.get(i).nodeID+" "+graph.V.get(i).centrality);
+			/*System.out.println("Centrality values: Using "+source+" as source vertex in this iteration -");
+			for(int i=0; i<graph.V.size(); i++){
+				System.out.println(i+" "+graph.V.get(i).nodeID+" "+graph.V.get(i).centrality);
+			}
+			System.out.println();
 			*/
 		}
-		return graph;
+	}
+	
+	public void removeInterClusterEdges(float threshold){
+		Thread[] threads = new Thread[NTHREAD];
+		List<DocNode> myList;
+		int share = (int)Math.ceil(graph.V.size()/NTHREAD);
+		Object lock=new Object();
+		for (int i = 0; i < NTHREAD; i++) {
+			if(i!=NTHREAD-1)
+				myList = (List<DocNode>)graph.V.subList(i*share, (i+1)*share);
+			else
+				myList = (List<DocNode>)graph.V.subList(i*share, graph.V.size());
+			threads[i] = new Thread(new EdgeRemoverRunnable(i, graph.adjList, myList, threshold, lock));
+			threads[i].start();
+		}
+		for (Thread thread : threads) {
+			try {
+				//A Wait for Joining all threads merging in a single Iteration
+				thread.join();
+
+			} catch (InterruptedException ee) {
+				ee.printStackTrace();
+			}
+		}
+		for(int i=0; i<graph.V.size(); i++){
+			List<Edge> list=graph.adjList.get(graph.V.get(i));
+			for(int j=0; j<list.size(); j++){
+				System.out.print("Edge ("+graph.V.get(i).nodeID+","+list.get(j).getDst().nodeID+") ");
+			}
+			System.out.println("");
+			//System.out.println(" ");
+		}
+		
+	}
+	public List<Cluster> formClusters(List<List<DocNode>> list){
+		Thread[] threads = new Thread[NTHREAD];
+		List<List<DocNode>> myList;
+		int share = (int)Math.ceil(list.size()/NTHREAD);
+		Object lock=new Object();
+		List<Cluster> clusters = new ArrayList<Cluster>();
+		int nodeNum = graph.V.size();
+		for (int i = 0; i < NTHREAD; i++) {
+			if(i!=NTHREAD-1)
+				myList = list.subList(i*share, (i+1)*share);
+			else
+				myList = list.subList(i*share, list.size());
+			threads[i] = new Thread(new FormingClustersRunnable(nodeNum, myList, lock, clusters));
+			nodeNum+=1;
+			threads[i].start();
+		}
+		for (Thread thread : threads) {
+			try {
+				//A Wait for Joining all threads merging in a single Iteration
+				thread.join();
+
+			} catch (InterruptedException ee) {
+				ee.printStackTrace();
+			}
+		}
+		return clusters;
 	}
 }
 
-class MyComparator implements Comparator{
+class LevelComparator implements Comparator{
 	public int compare(Object i, Object j){
-		if((Integer)i>(Integer)j)
+		if((int)i>(int)j)
 			return -1;
 		return 1;
 	}
 }
 
+class PriorityComparator implements Comparator{
+	public int compare(Object i, Object j){
+		if(((DocNode)i).priority<((DocNode)j).priority)
+			return -1;
+		return 1;
+	}
+}
 
 class ForwardPhaseRunnable implements Runnable{
 	int id;
@@ -169,29 +235,21 @@ class ForwardPhaseRunnable implements Runnable{
 		this.queues=queues;
 	}
 	public void run(){
-		/*int u = min.node;
-
-			// find the neighbours
-			if (neighbours[u] == null) {
-				continue;
-			}
-
-			for (int i = 0; i < neighbours[u].length; ++i) {
-				double alt = priorityObjectArray[u].priority + weights[u][i];
-				if (alt < priorityObjectArray[neighbours[u][i]].priority) {
-					priorityQueue.decreasePriority(priorityObjectArray[neighbours[u][i]], alt);
-					previous[neighbours[u][i]] = u;
-				}*/
 		if(queue!=null){
 			for(int i=0; i<graph.adjList.get(source).size(); i++){
 				DocNode v = (DocNode)graph.adjList.get(source).get(i).getDst();
+				/*if(id==3)
+					System.out.print("--"+v.nodeID+"--");*/
 				if(V.contains(v)){
 					float alt = graph.adjList.get(source).get(i).getWeight() + source.priority;
 					if(v.priority==Float.MAX_VALUE){
 						queue.add(v);
 					}
 					if(alt < v.priority){
-						queue.decreasePriority(v, alt);
+						//queue.decreasePriority(v, alt);
+						queue.remove(v);
+						v.priority=alt;
+						queue.add(v);
 						v.pred.clear();
 						v.sig = 0;
 					}
@@ -207,10 +265,8 @@ class ForwardPhaseRunnable implements Runnable{
 					}
 				}
 			}
-	
-			//minNodes[id]=queue.findMin();
+
 			queues[id]=queue;
-			//System.out.println("Thread "+id+": "+queues[id].findMin().nodeID);
 		}
 	}
 }
@@ -236,3 +292,59 @@ class BackwardPhaseRunnable implements Runnable{
 	}
 }
 
+class EdgeRemoverRunnable implements Runnable{
+	int id;
+	Map<DocNode, List<Edge>> adjList;
+	List<DocNode> myNodes;
+	float threshold;
+	Object lock;
+	public EdgeRemoverRunnable(int id, Map<DocNode, List<Edge>> adjList, List<DocNode> myList, float threshold, Object lock){
+		this.id=id;
+		this.adjList=adjList;
+		this.myNodes=myList;
+		this.threshold=threshold;
+		this.lock=lock;
+	}
+	public void run(){
+		List<Edge> toRemove=new LinkedList<Edge>();
+		for(int i=0; i<myNodes.size(); i++){
+			List<Edge> list=adjList.get(myNodes.get(i));
+			for(Edge e: list){
+				DocNode neighbour = (DocNode)(e.getDst());
+				if((neighbour.centrality>threshold)&&(myNodes.get(i).centrality>threshold))
+					toRemove.add(e);
+			}
+		}
+		synchronized(lock){
+			for(Edge e: toRemove){
+				adjList.get(e.getSrc()).remove(e);
+				//System.out.println("removing ("+e.getSrc().nodeID+","+e.getDst().nodeID+")");
+			}
+		}
+	}
+}
+
+
+class FormingClustersRunnable implements Runnable{
+	int id;
+	List<List<DocNode>> components;
+	Object lock;
+	List<Cluster> clusters;
+	public FormingClustersRunnable(int id, List<List<DocNode>> list, Object lock, List<Cluster> clusters){
+		this.id = id;
+		components = list;
+		this.lock = lock;
+		this.clusters = clusters;
+	}
+	public void run(){
+		List<Cluster> list = new ArrayList<Cluster>();
+		for(int i=0; i<components.size(); i++){
+			Cluster c = new Cluster(id+i);
+			c.formCluster(components.get(i));
+			list.add(c);
+		}
+		synchronized(lock){
+			clusters.addAll(list);
+		}
+	}
+}
