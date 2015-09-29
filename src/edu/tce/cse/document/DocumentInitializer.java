5b864a06f6c6aa684df4883857683692b8b69ea9
maswin <maswin.cool@gmail.com>
2015-09-29 10:37:05 +0530

    SVDReducer Bug Fix

--- src/edu/tce/cse/document/DocumentInitializer.java
+++ src/edu/tce/cse/document/DocumentInitializer.java
@@ -66,39 +66,66 @@ public class DocumentInitializer {
 
 	private void reduceTfIDF(){
 		List<double[]>[] localTfIdf = new ArrayList[1];
-		localTfIdf[0] = new ArrayList<double[]>();
+		localTfIdf[0] = new ArrayList<double[]>(documentList.size());
 		
-		List<double[]>[] globalTfIdf = new ArrayList[1];
-		globalTfIdf[0] = new ArrayList<double[]>();
 		
 		for( Document doc : documentList){
 			localTfIdf[0].add(doc.getTfIdf());
 		}
 		
-		MPI.COMM_WORLD.Reduce(localTfIdf, 0, 
-				globalTfIdf, 0, 1, MPI.OBJECT, new Op(new TfIdfReducer(),true), 0);
-		int size[] = new int[1];
-		size[0] = 0;
-		if(MPI.COMM_WORLD.Rank()==0){
-			size[0] = globalTfIdf[0].size();
-		}
-		MPI.COMM_WORLD.Bcast(size, 0, 1, MPI.INT, 0);
+		//MPI.COMM_WORLD.Reduce(localTfIdf, 0, 
+				//globalTfIdf, 0, 1, MPI.OBJECT, new Op(new TfIdfReducer(),false), 0);
+
 		
-		double[][] tfIdfMatrix = new double[size[0]][];
-		
+		double[][] tfIdfMatrix;
+		int index = 0;
 		if(MPI.COMM_WORLD.Rank() == 0){
-			int index = 0;
+			//Initialize globalTfIdf
+			List<double[]>[] globalTfIdf = new ArrayList[1];
+			globalTfIdf[0] = new ArrayList<double[]>(N);
+			
+			//Collect TfIdf
+			globalTfIdf[0].addAll(localTfIdf[0]);
+			for(int i=1;i<MPI.COMM_WORLD.Size();i++){
+				localTfIdf[0] = new ArrayList<double[]>();
+				MPI.COMM_WORLD.Recv(localTfIdf, 0, 1, MPI.OBJECT, i, i);
+				globalTfIdf[0].addAll(localTfIdf[0]);
+			}
+			
+			//Create TfIdfMatrix
+			tfIdfMatrix = new double[N][];
 			for(double[] tfIdf : globalTfIdf[0]){
 				tfIdfMatrix[index] = tfIdf;
 				index++;
 			}
-			SVDReducer svd = new SVDReducer();
+			
+			//Reduce TfIdf
+			SVDReducer svd = new SVDReducer(30);
 			tfIdfMatrix = svd.reduceTfIdf(tfIdfMatrix);
+			
+			//Send Reduced TfIdf
+			for(int i=1;i<MPI.COMM_WORLD.Size();i++){
+				int offset = (N / numberOfProcessors) * i;
+		        int count = (i != numberOfProcessors - 1)
+		                ? (N / numberOfProcessors) * (i + 1) - 1
+		                : N - 1;
+		       count = count - offset + 1;
+		       MPI.COMM_WORLD.Send(tfIdfMatrix, offset, count, MPI.OBJECT, i, i);
+			}
+		}else{
+			
+			//Send TfIdf
+			MPI.COMM_WORLD.Send(localTfIdf, 0, 1, MPI.OBJECT, 0, this.processorID);
+			
+			//Receive Reduced TfIdf
+			int count = this.endIndex - this.startIndex + 1;
+			tfIdfMatrix = new double[count][];
+			MPI.COMM_WORLD.Recv(tfIdfMatrix, 0, count, MPI.OBJECT, 0, this.processorID);
+			System.out.println(this.processorID+" "+documentList.size()+" "+tfIdfMatrix.length);
 		}
-		
-		MPI.COMM_WORLD.Bcast(tfIdfMatrix, 0, size[0], MPI.OBJECT, 0);
-		int index = this.startIndex;
+		index = 0;
 		for(Document doc : documentList){
+			//System.out.println(this.processorID+" "+tfIdfMatrix[index].length);
 			doc.setTfIdf(tfIdfMatrix[index]);
 			index++;
 		}
