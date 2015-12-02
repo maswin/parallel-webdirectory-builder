32bab58d21520bb907dd29f43d999f5093ecbf19
maswin <maswin.cool@gmail.com>
2015-12-02 15:42:23 +0530

    Additional Modules to DocMemManager

--- src/edu/tce/cse/document/DocMemManager.java
+++ src/edu/tce/cse/document/DocMemManager.java
@@ -9,26 +9,33 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import edu.tce.cse.clustering.Cluster;
+import edu.tce.cse.model.Centroid;
 
 public class DocMemManager {
 
 	public static int maxSize = 5000;
+	public static int totalSize = 10000;
 
 	private static LinkedHashMap<Long, Document> documentMap = new <Long, Document>LinkedHashMap(maxSize + 1, .75F, false) {
-		   protected boolean removeEldestEntry(Map.Entry eldest) {
-		   return size() > maxSize;
-		   }
-		   };
+		protected boolean removeEldestEntry(Map.Entry eldest) {
+			return size() > maxSize;
+		}
+	};
 	private static LinkedHashMap<Long, DocNode> docNodeMap = new <Long, DocNode>LinkedHashMap(maxSize + 1, .75F, false) {
-		   protected boolean removeEldestEntry(Map.Entry eldest) {
-		   return size() > maxSize;
-		   }
-		   };
+		protected boolean removeEldestEntry(Map.Entry eldest) {
+			return size() > maxSize;
+		}
+	};
 	private static LinkedHashMap<Long, Cluster> clusterMap = new <Long, Cluster>LinkedHashMap(maxSize + 1, .75F, false) {
-		   protected boolean removeEldestEntry(Map.Entry eldest) {
-		   return size() > maxSize;
-		   }
-		   };
+		protected boolean removeEldestEntry(Map.Entry eldest) {
+			return size() > maxSize;
+		}
+	};
+	private static LinkedHashMap<Long, Centroid> centroidMap = new <Long, Centroid>LinkedHashMap(maxSize + 1, .75F, false) {
+		protected boolean removeEldestEntry(Map.Entry eldest) {
+			return size() > maxSize;
+		}
+	};
 
 	//Read & Write Document
 	public static void writeDocument(Document doc) {
@@ -36,6 +43,7 @@ public class DocMemManager {
 			FileOutputStream fout = new FileOutputStream("var/"+doc.docID+"Doc.ser");
 			ObjectOutputStream oos = new ObjectOutputStream(fout);
 			oos.writeObject(doc);
+			checkSafety();
 			documentMap.put(doc.docID, doc);
 			oos.close();
 			fout.close();
@@ -54,6 +62,7 @@ public class DocMemManager {
 				FileInputStream fin = new FileInputStream("var/"+id+"Doc.ser");
 				ObjectInputStream ois = new ObjectInputStream(fin);
 				doc = (Document) ois.readObject();
+				checkSafety();
 				documentMap.put(id, doc);
 				return doc;
 			} catch (Exception e){
@@ -70,6 +79,7 @@ public class DocMemManager {
 			FileOutputStream fout = new FileOutputStream("var/"+doc.nodeID+"Node.ser");
 			ObjectOutputStream oos = new ObjectOutputStream(fout);
 			oos.writeObject(doc);
+			checkSafety();
 			docNodeMap.put(doc.nodeID, doc);
 			oos.close();
 			fout.close();
@@ -88,6 +98,7 @@ public class DocMemManager {
 				FileInputStream fin = new FileInputStream("var/"+id+"Node.ser");
 				ObjectInputStream ois = new ObjectInputStream(fin);
 				doc = (DocNode) ois.readObject();
+				checkSafety();
 				docNodeMap.put(id, doc);
 				return doc;
 			} catch (Exception e){
@@ -104,6 +115,7 @@ public class DocMemManager {
 			FileOutputStream fout = new FileOutputStream("var/"+c.nodeID+"Cluster.ser");
 			ObjectOutputStream oos = new ObjectOutputStream(fout);
 			oos.writeObject(c);
+			checkSafety();
 			clusterMap.put(c.nodeID, c);
 			oos.close();
 			fout.close();
@@ -122,6 +134,7 @@ public class DocMemManager {
 				FileInputStream fin = new FileInputStream("var/"+id+"Cluster.ser");
 				ObjectInputStream ois = new ObjectInputStream(fin);
 				c = (Cluster) ois.readObject();
+				checkSafety();
 				clusterMap.put(id, c);
 				return c;
 			} catch (Exception e){
@@ -132,40 +145,68 @@ public class DocMemManager {
 		return null;
 	}
 
-	/*private static boolean isNotSafe(){
-		if(documentMap.size()>=maxSize){
-			return true;
+	//Read & Write Centroid
+	public static void writeCentroid(Centroid c) {
+		try {
+			FileOutputStream fout = new FileOutputStream("var/"+c.clusterId+"Centroid.ser");
+			ObjectOutputStream oos = new ObjectOutputStream(fout);
+			oos.writeObject(c);
+			checkSafety();
+			centroidMap.put(c.clusterId, c);
+			oos.close();
+			fout.close();
+		} catch (Exception e){
+			System.out.println("Write Centroid Exception");
+			e.printStackTrace();
 		}
-		if(docNodeMap.size()>=maxSize){
-			return true;
+	}
+
+	public static Centroid getCentroid(long id){
+		Centroid c;
+		if(centroidMap.containsKey(id)){
+			return centroidMap.get(id);
+		}else{
+			try {
+				FileInputStream fin = new FileInputStream("var/"+id+"Centroid.ser");
+				ObjectInputStream ois = new ObjectInputStream(fin);
+				c = (Centroid) ois.readObject();
+				checkSafety();
+				centroidMap.put(id, c);
+				return c;
+			} catch (Exception e){
+				System.out.println("Read Centroid Exception");
+				e.printStackTrace();
+			}
 		}
-		if(clusterMap.size()>=maxSize){
-			return true;
+		return null;
+	}
+	private static void checkSafety(){
+		int size = documentMap.size()+docNodeMap.size()+clusterMap.size()+centroidMap.size();
+		if(size>totalSize){
+			flushMemory();
 		}
-		return false;
 	}
 
 	private static void flushMemory(){
 		flushDocument();
 		flushDocNode();
 		flushCluster();
+		flushCentroid();
+		System.gc();
 	}
 
 	private static void flushDocument(){
-		DocMemManager.documentMap.clear();
-		DocMemManager.documentMap = new <Long, Document>HashMap();
-		System.gc();
+		DocMemManager.documentMap.clear();	
 	}
 
 	private static void flushDocNode(){
 		DocMemManager.docNodeMap.clear();
-		DocMemManager.docNodeMap = new <Long, DocNode>HashMap();
-		System.gc();
 	}
 	private static void flushCluster(){
 		DocMemManager.clusterMap.clear();
-		DocMemManager.clusterMap = new <Long, Cluster>HashMap();
-		System.gc();
-	} */
+	}
+	private static void flushCentroid(){
+		DocMemManager.centroidMap.clear();
+	}
 
 }
